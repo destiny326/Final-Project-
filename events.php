898874c48aeff06d29e295c97d18f924cb121fe8
destiny326 <destinyrma4@gmail.com>
2025-12-12 09:22:24 -1200

    Create events.php

---
+++ events.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StagePass-Events</title>
+</head>
+<body>
+    <nav id="top-menu">
+    <ul>
+      <li><a href="tickets.php">My tickets</a></li>
+      <li><a href="Events.php">Events</a></li> 
+      <li><a href="bookings.php">Book Ticket</a></li>
+      <li><a href="Cart.php">Cart</a></li>
+      <li><a href="contact.php">Contact Us</a></li>
+      <li><a href="account.php"> Account </a></li>
+     <button onclick="window.location.href= 'login.php'"> LOGIN</button>
+    </ul>
+  </nav>
+  <!-- Event 1 -->
+  
+<figure class= "events">
+    <img src="" alt="">
+</figure>
+
+
+</body>
+</html>
