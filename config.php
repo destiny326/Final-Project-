5d86ce4398be8852ad086f088763857a10a817e7
destiny326 <destinyrma4@gmail.com>
2025-12-12 12:24:16 -1200

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials.*/
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_SERVER', 'mysql-6618668-destinyrma4-3d00.g.aivencloud.com');
+define('DB_USERNAME', 'avnadmin');
+define('DB_PASSWORD', 'AVNS_2QgIDfq3O9U2e_p9o5h');
 define('DB_NAME', 'stagedb');
-define('DB_PORT', '3306');
+define('DB_PORT', '22394');
  
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
  
